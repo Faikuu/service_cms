c9e174f362be332d790ffc88ba2b61498887d55a
Adam Stasiak <faiku@proton.me>
2024-09-03 23:07:06 +0200

    Switch bootstrap to tailwind css

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ import './bootstrap.js';
 import './styles/app.css';
 
 const $ = require('jquery');
-require('bootstrap');
-require('./styles/global.scss');
+// require('bootstrap');
+// require('./styles/global.scss');
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
