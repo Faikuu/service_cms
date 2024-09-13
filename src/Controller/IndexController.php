516f6f358140c87e1f4d9b99edc7a4ae634e8a29
Adam Stasiak <faiku@proton.me>
2024-09-13 23:23:34 +0200

    product listing on home

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\CatalogItem;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $items = $entityManager->getRepository(CatalogItem::class)->findAll();
+
         return $this->render('index/index.html.twig', [
+            'items' => $items,
             'controller_name' => 'IndexController',
         ]);
     }
