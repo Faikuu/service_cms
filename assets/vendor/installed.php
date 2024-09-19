3f65010538448f2d8670e8baecc67917eeeb8813
Adam <faiku@proton.me>
2024-09-19 16:41:50 +0200

    fix system freeze on fedora/rhel based systems

--- assets/vendor/installed.php
+++ assets/vendor/installed.php
@@ -9,6 +9,16 @@
     array (
     ),
   ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
   'tom-select/dist/css/tom-select.default.css' => 
   array (
     'version' => '2.3.1',
@@ -22,16 +32,6 @@
   'tom-select' => 
   array (
     'version' => '2.3.1',
-    'dependencies' => 
-    array (
-    ),
-    'extraFiles' => 
-    array (
-    ),
-  ),
-  '@hotwired/turbo' => 
-  array (
-    'version' => '7.3.0',
     'dependencies' => 
     array (
     ),
