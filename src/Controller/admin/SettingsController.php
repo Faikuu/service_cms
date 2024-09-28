7fdb95936a8631e464ae0d77f7c648fba9c6fcd9
Adam Stasiak <faiku@proton.me>
2024-09-28 18:42:19 +0200

    Add general settings controller

--- src/Controller/admin/SettingsController.php
+++ src/Controller/admin/SettingsController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\admin;
 
 use App\Entity\Options;
+use App\Form\GeneralSettingsFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,16 +14,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class SettingsController extends AbstractController
 {
     #[Route('/', name: 'app_adminsettings')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'IndexController',
+        $options = $entityManager->getRepository(Options::class)->findAll();
+    
+        $data = [];
+        foreach ($options as $option) {
+            $data[$option->getOptionName()] = $option->getOptionValue();
+        }
+    
+        $form = $this->createForm(GeneralSettingsFormType::class, $data);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            foreach ($form->getData() as $key => $value) {
+                $this->upsertOption($entityManager, $key, $value);
+            }
+    
+            $this->addFlash('success', 'Settings saved');
+    
+            return $this->redirectToRoute('app_adminsettings');
+        }
+    
+        return $this->render('admin/settings/index.html.twig', [
+            'form' => $form->createView(),
+            'options' => $options,
         ]);
     }
 
     private function upsertOption($entityManager, $key, $value)
     {
-        $option = $entityManager->getRepository(Options::class)->findOneBy(['key' => $key]);
+        $option = $entityManager->getRepository(Options::class)->findOneBy(['option_name' => $key]);
         if (!$option) {
             $option = new Options();
             $option->setOptionName($key);
